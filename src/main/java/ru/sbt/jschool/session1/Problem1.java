77be8975ab77324728641fe812259e4008521b9c
Evgeniy Safonov <rising.niar@gmail.com>
2018-03-20 21:50:21 +0300

    Первые 3 задачи.

--- src/main/java/ru/sbt/jschool/session1/Problem1.java
+++ src/main/java/ru/sbt/jschool/session1/Problem1.java
@@ -5,5 +5,8 @@ package ru.sbt.jschool.session1;
 public class Problem1 {
     public static void main(String[] args) {
 
+        // Написать программу, которая выводит все аргументы, которые ей передали.
+        for (int i = 0; i < args.length; i++)
+            System.out.println(args[i]);
     }
 }
